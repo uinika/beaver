a8ce1ac7c860446875c4b87147c9ed103ffc9591
hank.zheng <uinika@outlook.com>
2017-06-06 01:18:21 +0800

    Update

--- contents/articles/a.module.js
+++ contents/articles/a.module.js
@@ -18,22 +18,18 @@
         template: "<ui-view/>"
       })
       .state("articles.web", {
-        parent: "articles",
         url: "/web",
         templateUrl: "contents/articles/web/view.html"
       })
       .state("articles.electron", {
-        parent: "articles",
         url: "/electron",
         templateUrl: "contents/articles/electron/view.html"
       })
       .state("articles.design", {
-        parent: "articles",
         url: "/design",
         templateUrl: "contents/articles/design/view.html"
       })
       .state("articles.reading", {
-        parent: "articles",
         url: "/reading",
         templateUrl: "contents/articles/reading/view.html"
       })
